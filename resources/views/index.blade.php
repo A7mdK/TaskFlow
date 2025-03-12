e7514afd57021ea108463d8097511d3bfee5d758
Ahmed KHALIL <105590089+A7mdK@users.noreply.github.com>
2025-03-12 12:18:44 +0300

    Task Pagination Implementation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,12 @@
     @empty
         <div>There are no tasks!</div>
     @endforelse
+
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
 
 {{--
